69ca80107d0c591d01558aca8f188aea75218028
aldorax <climaxating@gmail.com>
2024-07-27 23:09:49 +0100

    UPDATE: Fixed UI
    Fixed a lot of stuff

--- customChain.ts
+++ customChain.ts
@@ -1,15 +1,15 @@
 import { defineChain } from "viem";
 
 export const ubit = defineChain({
-  id: 44433,
-  name: "Ubit Testnet",
+  id: 90002,
+  name: "UBIT SMARTCHAIN",
   iconUrl: "https://s2.coinmarketcap.com/static/img/coins/64x64/14914.png",
   iconBackground: "#fff",
   nativeCurrency: { name: "USC", symbol: "USC", decimals: 18 },
   rpcUrls: {
-    default: { http: ["https://testnet-rpc.ubitscan.io/"] },
+    default: { http: ["https://rpc.ubitscan.io/"] },
   },
   blockExplorers: {
-    default: { name: "UBITscan", url: "https://ubitscan.io/" },
+    default: { name: "UBITscan", url: "https://ubitscan.io" },
   },
 });
